6b788e4762f54eb3b1c368a710d8b1f6d41d27f4
Johanafuentes <Johafuentes869@gmail.com>
2025-11-29 21:20:54 -0500

    se midifico el suite de rutas

--- bot_hercule2/app/src/test/java/runners/SuiteRouters/SuiteRouters.java
+++ bot_hercule2/app/src/test/java/runners/SuiteRouters/SuiteRouters.java
@@ -3,9 +3,20 @@ package runners.SuiteRouters;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import runners.CreateVehicleRunner;
+import runners.views_and_search_routes;
+import runners.views_and_search_cities;
+import runners.ViewsAndSearchVehiculos;
+import runners.views_and_search_drivers;
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        CreateVehicleRunner.class
+        CreateVehicleRunner.class,
+        ViewsAndSearchVehiculos.class,
+        views_and_search_routes.class,
+        views_and_search_cities.class,
+        views_and_search_drivers.class,
+
+
 })
 public class SuiteRouters {
 }
